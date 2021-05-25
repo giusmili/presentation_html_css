db522b40fccbc0e0d070b43e96402066de4bae6f
giusmili <giusmili67@gmail.com>
2021-05-25 17:37:43 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,150 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    console.log(200)
+
+    class User{
+        nom="Gius";
+        prenom= "Mili";
+    }
+    
+    let affichage = new User; /* important l'instance */
+    console.log(affichage.nom)
+    /* 
+    schema de class
+    */
+    class Person{
+       constructor(nom, prenom, age){
+            this.nom = nom;
+            this.prenom = prenom;
+            this.age = age;
+        }
+        
+        //getter
+        getout(){
+            return this.prenom, this.nom;
+        }
+
+    }
+    const person_1 = new Person("Batman","Bruce", 25);
+    console.log(person_1.nom+' '+person_1.prenom+' '+person_1.age);
+ 
+    
+    
+    class Pres{
+        title = 'Classes html, js and css - 2021';
+        el = document.querySelector("title");
+        heading = document.querySelector("header h1");
+    }
+
+
+    let setTitle = new Pres;
+    console.log(setTitle.el)
+    setTitle.el.innerText = setTitle.title;
+    setTitle.heading.innerHTML = `<span aria-hidden="true">&#128077;</span>${setTitle.title}`;
+
+    /* objet */
+    console.log(typeof setTitle)
+    let Prod = {
+        marque : "Samsung",
+        prix : 750,
+        annee : 2021,
+        dispo: true,
+            getout(){
+                return this.marque+' '+this.prix;
+            }
+    }
+    /* pour affichder Samsung */
+    let affichages = Prod.getout();
+    console.log(affichages)
+
+    let aside_title = ({
+        title: "Page color palette",
+        title_el: document.querySelector("aside h3")
+    })
+    
+  console.log(aside_title.title)
+  aside_title.title_el.innerText += aside_title.title
+  /* les opérateurs
+  =
+  += ajouter
+  -= l'inverse d'ajouter
+  ++
+  --
+  !=
+  !==
+  == conditions
+  === différent de type et de valeur
+  <=
+  >=
+  ! négation
+  &&
+  ||
+  ?
+  */
+ 
+  let x = true
+  let y = false
+  console.log(!x==y ? true : false) //condition ternaire
+  console.log(!x)
+
+  if(!x){
+    console.log("ok")
+  } 
+  else{
+      console.log("ko")
+  }
+
+
+
+    let elements = document.querySelector("figcaption h2")
+    console.log(elements.innerText)
+    let personnage = { /* données  et DOM */
+        alias: "Flash",
+        first_name: "Barry",
+        speed: "?",
+        color: "red",
+        age: 25,
+        dates: new Date().getFullYear()
+
+    }
+    function Chrono () {
+        this.chronos = 0;
+      
+        setInterval(() => {
+          //console.log(this.chronos++);
+          //elements.innerText = this.chronos++;
+            
+          if(this.chronos >= 10){
+                //document.location.assign('https://www.google.com')
+                this.chronos = 0;
+                
+            }
+           
+
+         }, 1000);
+      
+
+      }
+ 
+      
+      
+      let p = new Chrono();
+ 
+    elements.innerText = `${personnage.alias} legend dc, age ${personnage.age} ans `
+    console.log(personnage.alias)
+    let footer_el = document.querySelector("footer p")
+    //let dates = new Date().getFullYear()
+
+    footer_el.innerHTML +=`${personnage.dates}`
+
+    const prix = 250;
+    const tva = 0.196 /* 19;6 */
+    console.log(250 * tva / 100)
+    ttc = 250 * tva / 100;
+    console.log(prix + ttc)
+    
+    
+        
+  
+    
+
+})
---
+++ readme.md
@@ -0,0 +1,52 @@
+![](favicon/apple-touch-icon.png "html 5")
+# Présentation html &amp; css
+
+Dans ce dépôt le sujet est comment mettre en forme un document
+<code>html et css.</code>
+
+```html
+<main>
+        <figure>
+            <img src="./asset/flash.jpg" alt="flash">
+            <figcaption>
+                <h2>Legende dc</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, incidunt?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore excepturi laudantium voluptatibus.
+                </p>
+            </figcaption>
+        </figure>
+    </main>
+
+```
+<code>côté css</code>
+
+```css
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap');
+html{
+  font-size: 62.5%;
+  background-color: #f2f3e7;
+}
+body{
+  font: 1.6rem 'Open Sans',sans-serif;
+  margin: 0;
+}
+h1,h2,h3,h4,p,ul,figure{
+  margin: 0;
+  list-style: none;
+  padding: 0;
+}
+
+/* theme */
+:root{
+    --color-primary: #120a1b;
+    --color-secondary: #d0d394;
+    --color-other: #ac6a35;
+    --color-last: #d6a121;
+}
+
+```
+## Nombre de fichiers
+* html
+* css
+* js
